2f024836e8edfb4c6fac6628fe09ec57008b6125
Your Name <fehmida1899@gmail.com>
2023-12-02 17:15:54 +0530

    collection

--- CollectionFramework-Implementation/src/com/collection/set/LinkedHashSet.java
+++ CollectionFramework-Implementation/src/com/collection/set/LinkedHashSet.java
@@ -17,91 +17,74 @@ public class LinkedHashSet {
 		/*
 		 * POINTS TO BE REMEMBER ABOUT LINKEDHASHSET
 		 * 
-		 * 1.store only unique elements 
-		 * 2.order of insertion is maintain 
-		 * 3.size is DYNAMIC 
-		 * 4.we can not access the elements by index (not index-basing
-		 * collection) 
-		 * 5.if we want to access elements of hashSet so first we have to
-		 * converted into arrayList 
-		 * 6.we can traverse both type safe or unType safe of
-		 * collections 
-		 * 7.it extends (child of) HASHSET class
+		 * 1.store only unique elements 2.order of insertion is maintain 3.size is
+		 * DYNAMIC 4.we can not access the elements by index (not index-basing
+		 * collection) 5.if we want to access elements of hashSet so first we have to
+		 * converted into arrayList 6.we can traverse both type safe or unType safe of
+		 * collections 7.it extends (child of) HASHSET class
 		 * 
 		 * 
 		 * 
 		 * THERE ARE TOTAL SIX METHOD FOR TRAVERSING IN LINKEDLIST
 		 * 
-		 * 1.using a for-each loop (ONLY SUPPORTED TYPE SAFE COLECTION) 
-		 * 2.using an iterator
-		 * 3.using the forEach() method
+		 * 1.using a for-each loop (ONLY SUPPORTED TYPE SAFE COLECTION) 2.using an
+		 * iterator 3.using the forEach() method
 		 */
 
-		
 		System.out.println();
 		System.out.println("*****Lets see about For-Each Loop for traversing****");
-		/*	1.using a for-each loop
+		/*
+		 * 1.using a for-each loop
+		 * 
+		 * here we use wrapper class because collection are only store objects and
+		 * wrapper class is the representation of primitives data type like int , char
+		 * and so on
+		 * 
+		 * when we add primitive data type in collections they automatically converted
+		 * into their respective Wrapper class and this process is called AUTO BOXING
+		 * 
+		 * and we access or use the element that are converted into their respective
+		 * Wrapper class they automatically converted into their original data type that
+		 * is primitive data type and this process is called AUTO UNBOXING
+		 */
 
-		here we use wrapper class because 
-		collection are only store objects
-		and wrapper class is the representation 
-		of primitives data type like int , char and so on
-
-		when we add primitive data type
-		in collections they automatically 
-		converted into their respective Wrapper class
-		and this process is called AUTO BOXING
-
-		and we access or use the element 
-		that are converted into their 
-		respective Wrapper class they 
-		automatically converted into
-		their original data type that is
-		primitive data type and this process
-		is called AUTO UNBOXING */
-		
 		System.out.println();
-		for(Integer el : set) {
+		for (Integer el : set) {
 			System.out.println(el);
 		}
-		
-		
-		
-		System.out.println("*****Lets see about Iterator for traversing****");
 
-		/* 2.using an iterator
 
-		for traversing through iterator first 
-		we have gain the object of iterator 
-		from collection interface
-		and after store into any variable 
-		we can only traversing in forward direction (left to right) */
-		
+		/*
+		 * 2.using an iterator
+		 * 
+		 * for traversing through iterator first we have gain the object of iterator
+		 * from collection interface and after store into any variable we can only
+		 * traversing in forward direction (left to right)
+		 */
+
 		Iterator<Integer> itr = set.iterator();
-		while(itr.hasNext()) {
+		while (itr.hasNext()) {
 			System.out.println(itr.next());
 		}
-		
-		//here hasNext is method which check that the element is null 
-		//or present
-		
-		
+
+		// here hasNext is method which check that the element is null
+		// or present
+
 		System.out.println("*****Lets see about For-Each Method for traversing****");
 
-		/*    3.using the forEach() method  (JAVA 8 FEATURE)
+		/*
+		 * 3.using the forEach() method (JAVA 8 FEATURE)
+		 * 
+		 * here we use lambda Expression for traversing we can give any name for e
+		 */
 
-		here we use lambda Expression for traversing
-		we can give any name for e */ 
-		
-		
-		set.forEach(e->{
+		set.forEach(e -> {
 			System.out.println(e);
 		});
-		
-		
+
 		System.out.println("*****Lets see about For-Each Method(UNTYPE SAFE) for traversing****");
-		//we can also create NON-TYPE SAFE linkedHashSet 
-		
+		// we can also create NON-TYPE SAFE linkedHashSet
+
 		java.util.LinkedHashSet link2 = new java.util.LinkedHashSet();
 
 		link2.add("Hina");
@@ -111,15 +94,9 @@ public class LinkedHashSet {
 		link2.add(true);
 		link2.add("Blue");
 
-		link2.forEach(el->{
+		link2.forEach(el -> {
 			System.out.println(el);
 		});
 
-		
-		
-		
-		
-		
-		
 	}
 }
